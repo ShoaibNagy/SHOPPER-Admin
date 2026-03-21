fbabff833b1cfd62f33eef199f6783a19049b800
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-21 12:36:29 +0200

    Add Sidebar component and styles, integrate into Admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar/Navbar";
+import Admin from "./pages/admin/Admin";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Admin />
     </>
   );
 }
--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,9 +1,10 @@
+import Sidebar from '../../components/sidebar/Sidebar';
 import './Admin.css';
 
 const Admin = () => {
   return (
     <div className="admin">
-      
+      <Sidebar />
     </div>
   );
 }
