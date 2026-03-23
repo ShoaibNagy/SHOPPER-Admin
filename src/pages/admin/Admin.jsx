61a033eb0787f835f813c37ca6010ac3fbbf3f04
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-03-23 23:10:54 +0200

    Add AddProduct and ListProduct components with styles, integrate into Admin page

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import Sidebar from '../../components/sidebar/Sidebar';
 import './Admin.css';
+import AddProduct from '../../components/addProduct/AddProduct';
+import ListProduct from '../../components/listProduct/ListProduct';
 
 const Admin = () => {
   return (
     <div className="admin">
       <Sidebar />
+      <Routes>
+        <Route path='/addproduct' element={<AddProduct />} />
+        <Route path='/listproduct' element={<ListProduct />} />
+      </Routes>
     </div>
   );
 }
